0a511fb5124b078cbc9542f46d2e3be3cbf7a18f
ashubgowda <ashubgowda06@gmail.com>
2026-01-31 01:29:16 +0530

    login form code

---
+++ Selenium Practice/src/com/practice2026/basics/Demo.java
@@ -0,0 +1,13 @@
+package com.practice2026.basics;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
